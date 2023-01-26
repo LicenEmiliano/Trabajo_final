2727afbf9d7fcb7236913478b248ec08b15a9184
LicenEmiliano <emilianolicen@hotmail.com>
2023-01-25 21:26:17 -0300

    javascript

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let video=document.querySelector(".video")
+/*let video=document.querySelector(".video")
 
 const iniciarRep=()=>{      
     video.play()
@@ -8,5 +8,59 @@ const iniciarRep=()=>{
 const pausarRep=()=>{      
     video.pause()
 }
+*/
+
+let menu= document.querySelector('.menu');
+let flag=false;
+let contador=0;
 
 
+function iniciar(){
+    let imagenes=document.querySelectorAll('#cajaimagenes img');
+    let soltar=document.getElementById('cajasoltar');  
+    let soltar2  =document.getElementById('cajasoltar2'); 
+    let soltar3 = document.getElementById('cajasoltar3'); 
+
+    for(let i=0; i<imagenes.length; i++){
+        imagenes[i].addEventListener('dragstart', arrastrado, false);        
+    }
+
+    soltar.addEventListener('dragenter', function(e){
+    e.preventDefault(); }, false);
+    soltar.addEventListener('dragover', function(e){
+    e.preventDefault(); }, false) ;
+    soltar.addEventListener('drop', soltado, false);
+
+
+    soltar2.addEventListener('dragenter', function(e){
+        e.preventDefault(); }, false);
+    soltar2.addEventListener('dragover', function(e){
+    e.preventDefault(); }, false);
+    soltar2.addEventListener('drop', soltado, false);
+
+
+    soltar3.addEventListener('dragenter', function(e){
+        e.preventDefault(); }, false);
+    soltar3.addEventListener('dragover', function(e){
+    e.preventDefault(); }, false);
+    soltar3.addEventListener('drop', soltado, false);
+
+}
+function arrastrado(e){
+    elemento=e.target;
+    e.dataTransfer.setData('Text', elemento.getAttribute('id'));
+}
+
+async function soltado(e){
+    e.preventDefault();    
+    let id=e.dataTransfer.getData('Text');
+    let imagen=document.getElementById(id);
+    imagen.style.display= 'none';
+    e.target.innerHTML='<img src="'+imagen.src+'" height="400px" width="275px">';
+    contador++
+
+
+}
+function reinicio() {
+    window.location.reload();
+}
